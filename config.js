81d17786da171649f0309eb19bfccd6036c85542
Ajibade Adewale Joseph <wallyjay111@gmail.com>
2025-01-23 10:03:25 -0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const global = {
+    owner: [5102885245], // your telegram account id, replace it inside the bracket
+    botToken: "6774887776:FG66t7gvhbmg-gfggg45ftFFY7", // your telegram account token, change the one i used here
+}
+
+module.exports = global;
